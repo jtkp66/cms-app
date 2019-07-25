8a6274351f545cfcdfa0e1aa073415b5efc1f7f7
Jamie Pham <jtkp66@gmail.com>
2019-07-25 12:14:31 -0700

    Implemented react frontend app

--- cmspeer/cmspeer/urls.py
+++ cmspeer/cmspeer/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include('frontend.urls')),
     path('', include('cases.urls')),
+    path('admin/', admin.site.urls),
 ]
